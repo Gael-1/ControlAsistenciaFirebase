780536d0fd5e3b52156a40c9ce63952950d10780
walth <woltz2204@gmail.com>
2026-02-24 14:37:26 -0600

    2022477
    se agergo
    lista de alumnos
    porcentaje de asistencia
    toma de lista
    pantala de mis grupos
    y de mas

--- app/src/main/java/com/equipo1/controlasistencia/MainActivity.kt
+++ app/src/main/java/com/equipo1/controlasistencia/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.equipo1.controlasistencia.ui.theme.ControlAsistenciaTheme
+import com.equipo1.controlasistencia.AppNavegacion
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
